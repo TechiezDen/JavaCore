adab0688b6ae9648caf4c4032027c07df02b03da
ngactive99 <ngactive99@gmail.com>
2022-08-10 11:29:20 +0530

    If else statements

---
+++ src/com/demo/control/ControlStatements.java
@@ -0,0 +1,20 @@
+package com.demo.control;
+
+public class ControlStatements {
+	public static void main(String[] args) {
+		int NishantAge =  26;
+		int AkshayAge = 26;
+		
+		if(NishantAge > AkshayAge) {
+			System.out.println("Nishant is elder than Akshay");
+			
+		} else if (NishantAge == AkshayAge) {
+			System.out.println("Both have the same age ");
+		}
+		
+		else {
+			System.out.println("Akshay is elder than nishant ");
+		}
+	}
+
+}
