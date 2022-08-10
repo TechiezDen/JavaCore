781b64a176bf2d36fad7e392fc6b9cb00221b123
ngactive99 <ngactive99@gmail.com>
2022-08-10 10:40:22 +0530

    variables

--- src/com/demo/Basics.java
+++ src/com/demo/Basics.java
@@ -1,11 +1,29 @@
 package com.demo;
 
 public class Basics {
+	void method() {
+		int RollNo = 875; // local variable 
+		System.out.println(RollNo);
+	}
 	
 	public static void main(String[] args) {
-		
-		
 		System.out.println("hello world");
+		
+		int ReqNo = 556551;
+		
+		System.out.println(ReqNo);// instance Variable
+		
+		int priceOfVeggies = 100;
+		int PriceOfBag = 500;
+		
+		int total = priceOfVeggies + PriceOfBag;
+		System.out.println("The total value is : " + total);
+		
+		
+	
+		
 	}
+	
+	
 
 }
