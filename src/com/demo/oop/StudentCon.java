e36da8898c1a22d1f34e491f108c1c0dfbd6a6f6
ngactive99 <ngactive99@gmail.com>
2022-08-11 15:49:47 +0530

    parametirized  constructor

--- src/com/demo/oop/StudentCon.java
+++ src/com/demo/oop/StudentCon.java
@@ -5,6 +5,13 @@ public class StudentCon {
 	int id;
 	String name;
 	String subject;
+	// parameterized constructor 
+	
+	StudentCon(int i, String n, String sub) {
+		id = i ;
+		name = n;
+		subject = sub;
+	}
 	
 	// method to display the id and the name  
 	void display() {
@@ -12,8 +19,8 @@ public class StudentCon {
 	}
 	
 	public static void main(String[] args) {
-		StudentCon s1 = new StudentCon();
-		StudentCon s2 = new StudentCon();
+		StudentCon s1 = new StudentCon(800, "Rishav", "Science");
+		StudentCon s2 = new StudentCon(801, "Peter", "English");
 		
 		s1.display();
 		s2.display();
